c2f873611db82d19f56ca4db21255712f0e7c37f
Etnath <kevin.giffone@gmail.com>
2017-05-05 09:41:28 +0200

    Add UnityModule

---
+++ Etnath.Utilities.Prism/UnityModule.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Etnath.Utilities.Prism
+{
+    public class UnityModule : IModule
+    {
+        IRegionManager _regionManager;
+        IUnityContainer _container;
+
+        public UnityModule(IUnityContainer container, IRegionManager regionManager)
+        {
+            _container = container;
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            
+        }
+    }
+}
