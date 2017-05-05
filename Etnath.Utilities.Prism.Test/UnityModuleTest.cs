617938c2908833ed8018772b7316aafc3c8c537a
Etnath <kevin.giffone@gmail.com>
2017-05-05 10:12:22 +0200

    Add test project Test Unity:odule initialization

---
+++ Etnath.Utilities.Prism.Test/UnityModuleTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prism;
+using Prism.Unity;
+using Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Etnath.Utilities.Prism;
+
+namespace Etnath.Utilities.Prism.Test
+{
+    [TestClass]
+    public class UnityModuleTest
+    {
+        TestBootstrapper bootstrapper;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            bootstrapper = new TestBootstrapper();
+            
+        }
+
+        [TestMethod]
+        public void UnityModuleInitTest()
+        {
+            Assert.IsNotNull(bootstrapper);
+
+            bootstrapper.Run();
+            bootstrapper.Container.RegisterType<IModule,UnityModule>();
+            var unityModule = bootstrapper.Container.Resolve<IModule>();
+
+            Assert.IsNotNull(unityModule);
+        }
+    }
+
+    public class TestBootstrapper : UnityBootstrapper
+    {
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            ModuleCatalog.AddModule(new ModuleInfo(
+                typeof(UnityModule).Name,
+                typeof(UnityModule).AssemblyQualifiedName));
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();       
+        }
+    }
+}
