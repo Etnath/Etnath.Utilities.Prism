b9719b3fe5c5af37bc73139525b5e99979e1bf3e
Etnath <kevin.giffone@gmail.com>
2017-05-27 05:35:17 +0200

    Add RegiterType For Navigation extensions methods

---
+++ Etnath.Utilities.Prism/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etnath.Utilities.Prism
+{
+    public static class ExtensionMethods
+    {
+        public static IUnityContainer RegisterTypeForNavigation<T>(this IUnityContainer container, string name) where T : class, IView, IViewModel 
+        {
+            return container.RegisterType<object, T>(name);
+        }
+
+        public static IUnityContainer RegisterTypeForNavigation<T>(this IUnityContainer container) where T : class, IView, IViewModel
+        {
+            return container.RegisterType<object, T>();
+        }
+    }
+}
